e5c2770b64695e5e0fbe98727b10b3f60195c87a
Shoeboxam <shoeboxam@gmail.com>
2017-03-11 00:37:38 -0600

    Clean up derivative accumulator
    I don't have a method for taking the matrix derivative involving basis functions. Not sure how this can be fixed

--- Neural.py
+++ Neural.py
@@ -13,7 +13,7 @@ class Neural(object):
     def __init__(self, units, basis=Function.basis_sigmoid, delta=Function.delta_linear, gamma=1e-1, epsilon=1e-4):
         self._weights = []
         for i in range(len(units) - 1):
-            self._weights.append(np.random.rand(units[i], units[i+1]))
+            self._weights.append(np.random.rand(units[i], units[i+1]) * 0.1 - 0.5)
 
         self._basis = basis
         self._delta = delta
@@ -47,28 +47,31 @@ class Neural(object):
         return data
 
     def train(self, data, expectation):
-        print(self.basis)
         converged = False
         while not converged:
             for layer in range(len(self._weights)):
-                # Where dx denotes matrix accumulation
-                dx_dWvec = np.kron(np.ndarray.flatten(data),
-                                   np.identity(np.size(np.matmul(np.transpose(self._weights[layer]), data))))
+                prediction = np.matmul(np.transpose(self.weights[layer]), data)
+                # dx is used to denote the beginning of the accumulation
+                # dr_dWvec =       S'                x I
+                dx_dWvec = np.kron(np.transpose(data), np.identity(np.size(prediction)))
 
                 # Accumulate derivative through all hidden layers
                 for i in range(layer, len(self._weights)):
-                    r = np.matmul(self._weights[i], data)
-                    dx_dWvec = np.matmul(np.matmul(np.ndarray.flatten(self._weights[i]),
-                                                   np.diag(self._basis.prime([r]))),
-                                         dx_dWvec)
+                    prediction = np.matmul(np.transpose(self._weights[i]), data) # y = W's
+
+                    # This fails because it is taking the scalar derivative when a matrix derivative is needed.
+                    # accumulator =      df_dh          * dh_dr                                  * existing
+                    dx_dWvec = np.matmul(self.weights[i], np.diag(self.basis.prime([prediction]))).matmul(dx_dWvec)
 
                 # Final error calculation
-                dln_dWvec = np.matmul(self._delta(expectation, self.evaluate(data)), dx_dWvec)
+                # accumulator =       dln_df                                              * existing
+                dln_dWvec = np.matmul(self.delta.prime([expectation, self.evaluate(data)]), dx_dWvec)
 
                 # Update weights
-                self._weights[layer] = self._weights[layer] - self._gamma * dln_dWvec
+                self.weights[layer] = self._weights[layer] - self.gamma * dln_dWvec
 
+            # Exit condition
             if (expectation - self.evaluate(data))**2 < self._epsilon:
                 converged = True
 
-                    # This is not tested. Going to have to think about how to clean it up.
+                    # This has many bugs. I'm working on cleaning it up. Suggestions welcome.
