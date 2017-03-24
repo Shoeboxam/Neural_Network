Add bias units, fix nan propagation
The magnitude of the initial random weights are relative to the size of the matrix, which prevents overflows.

App.py
@@ -5,7 +5,7 @@ from Function import *
 np.set_printoptions(suppress=True)
 
 # ~~~~Learning machine parameters~~~~
-layers = [2, 23, 12, 1]       # Number of nodes per layer
+layers = [2, 12, 23, 1]       # Number of nodes per layer
 
 S = np.array([[1, 1],       # Stimuli data  (environment)
               [0, 1],
@@ -15,7 +15,7 @@ S = np.array([[1, 1],       # Stimuli data  (environment)
 O = np.array([0, 1, 1, 0])  # Output data (expectation)
 
 # Create the net
-net = Neural(layers, delta=delta_linear, basis=basis_sinc, gamma=[.01, .01, .01], debug=True)
+net = Neural(layers, delta=delta_linear, basis=basis_sigmoid, debug=True)
 
 net.train(S, O)
 print(net.evaluate(S.T))

Neural.py
@@ -18,8 +18,8 @@ class Neural(object):
                  gamma=1e-2, epsilon=1e-2, debug=False):
         self._weights = []
         for i in range(len(units) - 1):
-            self._weights.append((np.random.rand(units[i+1], units[i]) - .5) * 2)
-        self._weights.append(np.array([[1]]))  # Dummy layer for ln
+            self._weights.append((np.random.rand(units[i+1], units[i]) - .5) * 25/np.sqrt(units[i]))
+        self._weights[0] = np.c_[self._weights[0], np.zeros(units[1])]
 
         self.basis = basis
         self.delta = delta
@@ -37,6 +37,11 @@ class Neural(object):
 
     def evaluate(self, data, depth=-1):
         # Depth can limit evaluation to a certain number of layers in the net
+        if np.ndim(data) == 1:
+            data = np.r_[data, 1]
+        else:
+            data = np.r_[data, np.ones([1, np.shape(data)[np.ndim(data)-1]])]
+
         if depth == -1:
             depth = len(self._weights)
 