Correct softmax and cross entropy, switch MNIST labels to one-hot

Environment.py
@@ -134,7 +134,12 @@ class MNIST(Environment):
     def _load_labels(file_name):
         with open(file_name, 'rb') as label_file:
             label_file.read(8)  # Ignore header info for file type and number of entries
-            return np.fromfile(label_file, dtype=np.int8)
+            integer_matrix = np.fromfile(label_file, dtype=np.int8)
+
+            # Convert to one-hot format
+            bool_matrix = np.zeros((np.size(integer_matrix), 10))
+            bool_matrix[np.arange(np.size(integer_matrix)), integer_matrix] = True
+            return bool_matrix
 
     def sample(self):
         x = np.random.randint(np.size(self.train_images[0]))

Function.py
@@ -19,46 +19,62 @@ class Function(object):
 
 # PARAMETERS
 tau   = 1            # Sigmoid threshold unit
-alpha = 0.5            # Parametrized rectified linear unit
+alpha = 0.5          # Parametrized rectified linear unit
 bank = 50            # Inverse learning function steepness
 
 # BASIS FUNCTIONS: Regression
 basis_identity  = Function('basis', 'identity',
-                           [lambda x: x, lambda x: np.ones(np.shape(x))])
+                           [lambda x: x,
+                            lambda x: np.diag(np.ones(np.shape(x)))])
 basis_binary    = Function('basis', 'binary',
-                           [lambda x: piecewise(x, 0, 1), lambda x: np.zeros(np.shape(x))])
+                           [lambda x: piecewise(x, 0, 1),
+                            lambda x: np.diag(np.zeros(np.shape(x)))])
 basis_relu      = Function('basis', 'relu',
-                           [lambda x: piecewise(x, alpha * x, x), lambda x: piecewise(x, alpha, 1)])
+                           [lambda x: piecewise(x, alpha * x, x),
+                            lambda x: np.diag(piecewise(x, alpha, 1))])
 basis_exponent  = Function('basis', 'exponent',
                            [lambda x: piecewise(x, alpha*(np.exp(x) - 1), x),
-                            lambda x: piecewise(x, alpha*np.exp(x), np.ones(np.shape(x)))])
+                            lambda x: np.diag(piecewise(x, alpha*np.exp(x), np.ones(np.shape(x))))])
 
 basis_logistic  = Function('basis', 'logistic',
-                           [lambda x: tau * (1 + np.exp(-x/tau))**-1, lambda x: np.exp(x/tau)/(np.exp(x/tau) + 1)**2])
+                           [lambda x: tau * (1 + np.exp(-x/tau))**-1,
+                            lambda x: np.diag(np.exp(x/tau)/(np.exp(x/tau) + 1)**2)])
 basis_softplus  = Function('basis', 'softplus',
-                           [lambda x: np.log(1 + np.exp(x)), lambda x: (1 + np.exp(-x))**-1])
+                           [lambda x: np.log(1 + np.exp(x)),
+                            lambda x: np.diag((1 + np.exp(-x))**-1)])
 basis_gaussian  = Function('basis', 'gaussian',
-                           [lambda x: np.exp(-x**2), lambda x: -2 * x * np.exp(-x**2)])
+                           [lambda x: np.exp(-x**2),
+                            lambda x: np.diag(-2 * x * np.exp(-x**2))])
 
 basis_tanh      = Function('basis', 'tanh',
-                           [lambda x: np.tanh(x), lambda x: 1 - np.tanh(x)**2])
+                           [lambda x: np.tanh(x),
+                            lambda x: np.diag(1 - np.tanh(x)**2)])
 basis_arctan    = Function('basis', 'arctan',
-                           [lambda x: np.arctan(x), lambda x: 1 / (x**2 + 1)])
+                           [lambda x: np.arctan(x),
+                            lambda x: np.diag(1 / (x**2 + 1))])
 basis_sinusoid  = Function('basis', 'sinusoid',
-                           [lambda x: np.sin(x), lambda x: np.cos(x)])
+                           [lambda x: np.sin(x),
+                            lambda x: np.diag(np.cos(x))])
 basis_sinc      = Function('basis', 'sinc',
                            [lambda x: piecewise_origin(x, np.sin(x) / x, 0),
-                            lambda x: piecewise_origin(x, np.cos(x) / x - np.sin(x) / x**2, 0)])
+                            lambda x: np.diag(piecewise_origin(x, np.cos(x) / x - np.sin(x) / x**2, 0))])
 
 basis_softsign  = Function('basis', 'softsign',
-                           [lambda x: x / (1 + np.abs(x)), lambda x: 1 / (1 + np.abs(x))**2])
+                           [lambda x: x / (1 + np.abs(x)),
+                            lambda x: np.diag(1 / (1 + np.abs(x))**2)])
 basis_bent      = Function('basis', 'bent',
-                           [lambda x: (np.sqrt(x**2 + 1) - 1) / 2 + x, lambda x: x / (2*np.sqrt(x**2 + 1)) + 1])
+                           [lambda x: (np.sqrt(x**2 + 1) - 1) / 2 + x,
+                            lambda x: np.diag(x / (2*np.sqrt(x**2 + 1)) + 1)])
 
 
 # BASIS FUNCTIONS: Classification
+def softmax(x):
+    temp = np.exp(x - x.max())
+    return temp / np.sum(temp)
+
 basis_softmax   = Function('basis', 'SMax',
-                           [lambda O, P: (np.exp(P) / np.sum(np.exp(P)))])
+                           [softmax,
+                            lambda x: softmax(x) * (np.eye(len(x)) - softmax(x))])
 
 
 # DELTA FUNCTIONS
@@ -67,8 +83,8 @@ delta_sum_squared    = Function('delta', 'SSE',  # Same as RSS and SSR
                             lambda O, P: -2 * np.transpose(O - P)])
 
 delta_cross_entropy  = Function('delta', 'CEE',
-                           [lambda O, P: (O * np.log(basis_sigmoid(P))) + (1 - O) * np.log(1 - basis_sigmoid(P)),
-                            lambda O, P: (basis_sigmoid(P) - O)])
+                           [lambda O, P: (O * np.log(P)) + (1 - O) * np.log(1 - P),
+                            lambda O, P: (P - O)])
 
 # REGULARIZATION DECAY FUNCTIONS
 decay_L1   = Function('decay', 'L1',

Neural.py
@@ -64,21 +64,21 @@ class Neural(object):
         # --- Setup parameters ---
 
         # Learning parameters
-        if type(learn_step) is float:
+        if type(learn_step) is float or type(learn_step) is int:
             learn_step = [learn_step] * len(self.weights)
 
         # Decay parameters
         if decay_step is None:
             decay_step = learn_step
 
-        if type(decay_step) is float:
+        if type(decay_step) is float or type(decay_step) is int:
             decay_step = [decay_step] * len(self.weights)
 
         # Moment parameters
         if moment_step is None:
             moment_step = learn_step
 
-        if type(moment_step) is float:
+        if type(moment_step) is float or type(moment_step) is int:
             moment_step = [moment_step] * len(self.weights)
 
         # --- Define propagation within net ---
@@ -172,7 +172,7 @@ class Neural(object):
                 r = self.weights[layer] @ s + self.biases[layer]
 
                 # Basis function derivative
-                dq_dr = np.diag(self.basis[layer](r, d=1))
+                dq_dr = self.basis[layer](r, d=1)
 
                 # Reinforcement function derivative
                 dr_dWvec = np.kron(np.identity(np.shape(self.weights[layer])[0]), s.T)
@@ -219,15 +219,16 @@ class Neural(object):
 
             # --- Debugging and graphing ---
             if debug:
-                print("Iteration: " + str(iteration))
-                print(self.predict(stimulus)[0] * 10)
-                print(expect)
+                predict_id = str(np.argmax(self.predict(stimulus)))
+                expect_id = str(np.argmax(expect))
+                equality = str(predict_id == expect_id)
+                print("Iteration: " + str(iteration) + ' P:O::' + predict_id + ':' + expect_id + '->' + equality)
 
             # Exit condition
             if iteration_limit is not None and iteration >= iteration_limit:
                 break
 
-            if graph or (epsilon and iteration % 50 == 0):
+            if (graph or epsilon) and iteration % 50 == 0:
                 [inputs, expectation] = map(np.array, environment.survey())
                 evaluation = self.predict(inputs.T)[0]
                 difference = np.linalg.norm(expectation.T - evaluation)