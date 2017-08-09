ccd6aa08b86fd41ae48275e47d449c99904b863d
Shoeboxam <shoeboxam@gmail.com>
2017-08-09 14:35:58 -0500

    Track parents via init

--- Jacobian_Chain/Layer.py
+++ Jacobian_Chain/Layer.py
@@ -4,16 +4,24 @@ from .Function import *
 
 class Layer(object):
     def __init__(self, children):
+
+        # Remember children
         if type(children) is not list:
             children = [children]
         self.children = children
+
+        # Store link to parent in child
+        for child in children:
+            child.parents.append(self.__class__)
+        self.parents = []
+
         self._iteration = 0
         self._forward_cache = []
         self._backward_cache = []
 
 
-class Reinforcement(Layer):
-    name = 'Reinforcement'
+class Transform(Layer):
+    name = 'transform'
 
     def __init__(self, children, nodes, distribute=dist_normal):
         super().__init__(children)
@@ -27,24 +35,41 @@ class Reinforcement(Layer):
         # Initialize weight matrix
         self.weights = distribute(sum(self.output_nodes), sum(self.input_nodes) + 1) * np.sqrt(2 / self.nodes)
 
-    def __call__(self, i, stimuli, d=0):
+        self.output_slices = []
+        cursor = 0
+        for sibling_id in range(len(self.output_nodes)):
+            space = self.output_nodes[sibling_id]
+            self.output_slices.append(slice(cursor, cursor + space))
+            cursor += space
+
+        self.input_slices = []
+        cursor = 0
+        for sibling_id in range(len(self.input_nodes)):
+            space = self.input_nodes[sibling_id]
+            self.input_slices.append(slice(cursor, cursor + space))
+            cursor += space
+
+    def __call__(self, caller, i, stimulus, deriv=None):
+        parent_id = self.parents.index(caller)
+
         if self._iteration == i:
-            if not d:
+            if not deriv:
                 return self._forward_cache
             return self._backward_cache
 
-        if forward:
-            stimulus = np.vstack([child(stimuli) for child in self.children])
-            return self.weights @ stimulus
+        if not deriv:
+            stimulus = np.vstack([child(stimulus) for child in self.children])
+            self._forward_cache = self.weights @ stimulus
+            return self._forward_cache[self.output_slices[parent_id], -1]
         else:
-            return self.weights[..., :-1]
+            return self.weights[self.output_slices[parent_id], -1]
 
     @staticmethod
     def node_search(children):
         """Count the number of input nodes"""
         node_count = 0
         for child in children:
-            if child.name is 'reinforcement':
+            if child.name is 'transform':
                 node_count += child.nodes
             elif child.name is 'stimulus':
                 node_count += child.nodes
