8b32861bae89a7a32b11d412a80ab0bda389335c
Shoeboxam <shoeboxam@gmail.com>
2017-09-02 23:38:55 -0500

    Stimulus tagging, removed iteration tracking
    Network to be called with [('tag', stimulus), ...] list. Each stimulus is tagged with its environment, so it can be identified once it recursively finds the start of the network.

--- MFP_Graph/Gate.py
+++ MFP_Graph/Gate.py
@@ -2,16 +2,15 @@ import numpy as np
 from .Variable import Variable
 
 
-# Return cached value if already computed in current iteration
+# Return cached value if already computed for current stimulus
 def _cache(method):
     def decorator(self, **kwargs):
-        if kwargs['i'] and self._iteration == kwargs['i']:
+        if self._cached_stimulus == kwargs['stimulus']:
+            self._cached_stimulus = kwargs['stimulus']
             return lambda **kw: getattr(self, '_cached_' + method.__name__)
         feature = method(self, **kwargs)
 
-        if kwargs['i']:
-            setattr(self, '_cached_' + method.__name__, feature)
-            setattr(self, '_iteration', kwargs['i'])
+        setattr(self, '_cached_' + method.__name__, feature)
         return feature
 
     return decorator
@@ -26,17 +25,17 @@ class Gate(object):
         # Stores references to variables in the gate
         self._variables = {}
 
-        self._iteration = 0
+        self._cached_stimulus = []
         self._cached___call__ = None
         self._cached_gradient = {}
 
-    def __call__(self, stimulus, i=None):
+    def __call__(self, stimulus):
         return np.vstack([child(stimulus, self) for child in self.children])
 
-    def gradient(self, grad, stimulus, variable):
+    def gradient(self, grad, variable, stimulus):
         if variable not in self.variables():
             # TODO: This shape may be incorrect
-            return grad @ np.zeros(self(stimulus).shape)
+            return grad @ np.zeros(self(i).shape)
         return grad @ np.vstack([child.gradient(stimulus, variable[idx]) for idx, child in enumerate(self.children)])
 
     @property
@@ -76,48 +75,44 @@ class Transform(Gate):
         return self.weights.shape[0]
 
     @_cache
-    def __call__(self, stimulus, i=None):
-        bias = np.ones([1, stimulus.shape[1]])
+    def __call__(self, stimulus):
+        return self.variables['weights'] @ super()(stimulus) + self.variables['biases']
 
-        propagated = super()(stimulus)
-        return np.vstack([propagated, bias]) @ self.weights
-
-    @property
     @_cache
-    def gradient(self, grad, stimulus, variable):
+    def gradient(self, grad, variable, stimulus):
         propagated = super()(stimulus)
-        if variable is self.weights:
+        if variable is self.variables['weights']:
             # Full derivative: This is a tensor product simplification to avoid the use of the kron product
             return grad.T @ propagated
-        if variable is self.biases:
+        if variable is self.variables['biases']:
             return grad
         return super().gradient(grad @ self.weights, stimulus, variable)
 
 
 class Logistic(Gate):
     @_cache
-    def __call__(self, stimulus, i=None):
+    def __call__(self, stimulus):
         features = super()(stimulus)
         return 1.0 / (1.0 + np.exp(-features))
 
-    @property
     @_cache
-    def gradient(self, grad, stimulus, variable):
+    def gradient(self, grad, variable, stimulus):
         return super().gradient(grad * self(stimulus) * (1.0 - self(stimulus)), stimulus, variable)
-
 
 
 class Stimulus(Gate):
     def __init__(self, environment):
-        super().__init__(self)
-        if type(environment) is not list:
-            environment = [environment]
+        super().__init__(children=[])
         self.environment = environment
 
     @_cache
-    def __call__(self, stimulus, i=None):
-        return self.environment[stimulus].sample()
+    def __call__(self, stimulus):
+        return stimulus[self.environment.tag]
 
     @property
-    def gradient(self, grad, stimulus, variable):
+    def gradient(self, grad, variable, stimulus):
         return np.eye(variable.shape[1])
+
+    @property
+    def output_nodes(self):
+        return self.environment[stimulus].size_output()
